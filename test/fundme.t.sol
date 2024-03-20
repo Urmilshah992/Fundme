700f48452d9fa7a0e1e32f234541de283ec35ffe
root <root@DESKTOP-PI8J1VA>
2024-03-20 16:43:08 +0530

    update files

---
+++ test/fundme.t.sol
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
+import {Test, console} from "forge-std/Test.sol";
+import {FundMe} from "../src/FundMe.sol";
+
+contract testFundme is Test {
+    FundMe fundme;
+
+    function setUp() external {
+        fundme = new FundMe(0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266);
+    }
+
+    function testmininmumUSD() public view {
+        assertEq(fundme.MINIMUM_USD(), 5e18);
+    }
+}
